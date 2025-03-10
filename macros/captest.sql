244c92c16aaa9d9e1290adeb9620228d2eeb13fe
krishna ginka <krishna.ginka27@gmail.com>
2025-03-10 07:12:18 +0000

    FFHJ

--- macros/captest.sql
+++ macros/captest.sql
@@ -1,7 +1,7 @@
-{% test captest(model, column_name) %}
+{% test captest27(model, column_name) %}
 
 select * From {{ ref('model') }}
-where {{column_name}} is not null
+where {{column_name}} is not null ;
 
 {% endtest %}
 
